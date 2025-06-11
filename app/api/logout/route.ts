01ccbb0d4a4878ab2cbf1b875732b14440473dc1
mxn890 <muskanwork74@gmail.com>
2025-06-11 21:31:15 +0500

    changes

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -2,6 +2,7 @@
 import { NextResponse } from 'next/server';
 import { serialize } from 'cookie';
 
+
 export async function POST() {
   const cookie = serialize('auth_token', '', {
     httpOnly: true,
