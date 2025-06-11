b7a60d592a08a87b1008b2d26a357876c96db2f1
mxn890 <muskanwork74@gmail.com>
2025-06-12 03:59:08 +0500

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
       domains: ["images.unsplash.com"], // add allowed external image domains here
     },
+    eslint:{
+        ignoreDuringBuilds: true,
+    }
   };
   
   export default nextConfig;
